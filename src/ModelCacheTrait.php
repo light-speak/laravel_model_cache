541d29b952885b0ed3051ec4634e45be4c6266ba
linty <linty@lintyone.cn>
2021-12-11 18:33:20 +0800

    修改注释

--- src/ModelCacheTrait.php
+++ src/ModelCacheTrait.php
@@ -2,18 +2,14 @@
 
 namespace LightSpeak\ModelCache;
 
-use Illuminate\Database\Eloquent\Model;
-use Psr\SimpleCache\InvalidArgumentException;
-
 trait ModelCacheTrait
 {
     /**
      * @param bool $useTransaction 是否使用事务，使用的话必须调用saveCache方法才可保存
      *
-     * @return CacheModel|ModelCacheTrait|mixed
-     * @throws InvalidArgumentException
+     * @return CacheModel
      */
-    public function cache(bool $useTransaction = false)
+    public function cache(bool $useTransaction = false): CacheModel
     {
         return ModelCache::make($this, __CLASS__, $useTransaction);
     }
